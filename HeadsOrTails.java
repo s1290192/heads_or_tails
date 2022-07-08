3d8cad18e81468d128cf166f63a7388a77e4d9cc
s1290192 <s1290192@std5dc46.u-aizu.ac.jp>
2022-07-08 11:38:43 +0900

    HoT:initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class HeadsOrTails
+{
+    public static void main(String[] args)
+    {
+	int coin, count_h = 0, count_t = 0;
+	Random ran = new Random();
+	System.out.println("Tossing coin...");
+	for(int i = 1; i <= 3; i++)
+	    {
+		System.out.print("Round " + i + ": ");
+		coin = ran.nextInt(2);
+
+		if(coin == 0)
+		    {
+			System.out.println("Tails");
+			count_t++;
+		    }
+		else
+		    {
+			System.out.println("Heads");
+			count_h++;
+		    }
+	    }
+	System.out.println("Heads: " + count_h + ", Tails: " + count_t);
+    }
+}
